f19231b39ec1faa910fcf66f095eb49c835bf303
ronaldodoi <arthurpolima@gmail.com>
2017-02-22 22:26:07 -0300

    Ajustando a corrotina na lava

---
+++ Assets/Scripts/OscilaZ.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OscilaZ : MonoBehaviour {
+
+    public float Velocidade, min, max, espera;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Move(max));
+	}
+	
+    IEnumerator Move(float destino)
+    {
+        while(Mathf.Abs(destino-transform.localPosition.z) > 0.2f)
+        {
+            Vector3 direcao = (destino == max) ? Vector3.forward : Vector3.back;
+            Vector3 velocidadevetorial = direcao * Velocidade;
+            transform.localPosition = transform.localPosition + velocidadevetorial * Time.deltaTime;
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(espera);
+
+        destino = (destino == max) ? min : max;
+        StartCoroutine(Move(destino));
+    }
+	
+}
