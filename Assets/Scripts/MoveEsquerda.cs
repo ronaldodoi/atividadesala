4888cb33536d37ca81b5cebd512ce521f0b810f6
ronaldodoi <arthurpolima@gmail.com>
2017-02-22 22:00:50 -0300

    Implementando movimento e retorno na prancha

---
+++ Assets/Scripts/MoveEsquerda.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveEsquerda : MonoBehaviour
+{
+
+    public float velocidade = 1f;
+    public float limite;
+    public float retorno;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 velocidadevetorial = Vector3.left * velocidade;
+
+        transform.position = transform.position + velocidadevetorial * Time.deltaTime;
+
+        if (transform.position.x <= limite)
+        {
+            transform.position = new Vector3(retorno, transform.position.y, transform.position.z);
+        }
+    }
+}
